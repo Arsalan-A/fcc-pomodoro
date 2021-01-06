166b7df068c7634c2eee62a90e4ceaf1f87f4267
Arz <zarsalan19@gmail.com>
2021-01-06 04:03:42 -0500

    implemented design

--- src/Pomodoro.js
+++ src/Pomodoro.js
@@ -1,34 +1,45 @@
 import React from 'react';
+import { BsCaretUpFill } from 'react-icons/bs';
+import { BsCaretDownFill } from 'react-icons/bs';
 
 function Pomodoro() {
   return (
-    <div className='container'>
-      <div className='timer-container'>
-        <label id='timer-label' htmlFor='timer'>
-          Session
-        </label>
-        <input name='timer' type='text' value='25:00' disabled />
-      </div>
+    <div className='bg-container'>
+      <div className='container'>
+        <div className='timer-container'>
+          <label id='timer-label' htmlFor='timer'>
+            Session
+          </label>
+          <input name='timer' type='text' value='25:00' disabled />
+          <div className='btn-container'>
+            <button id='start_stop'>Start</button>
+            <button id='reset'>Reset</button>
+          </div>
+        </div>
 
-      <div className='btn-container'>
-        <button id='start_stop'>Start</button>
-        <button id='reset'>Reset</button>
-      </div>
+        <div className='break-container'>
+          <label htmlFor='break' id='break-label'>
+            Break Length
+          </label>
+          <button id='break-increment'>
+            <BsCaretUpFill />
+          </button>
+          <input name='break' type='text' value='5' disabled />
+          <button id='break-decrement'>
+            <BsCaretDownFill />
+          </button>
+        </div>
 
-      <div className='break-container'>
-        <label htmlFor='break' id='break-label'>
-          Break Length
-        </label>
-        <button id='break-increment'>Increment</button>
-        <input name='break' type='text' value='5' disabled />
-        <button id='break-decrement'>Decrement</button>
-      </div>
-
-      <div className='session-container'>
-        <label htmlFor='session-label'>Session Length</label>
-        <button id='session-increment'>Increment</button>
-        <input name='session' type='text' value='25' disabled />
-        <button id='session-decrement'>Decrement</button>
+        <div className='session-container'>
+          <label htmlFor='session-label'>Session Length</label>
+          <button id='session-increment'>
+            <BsCaretUpFill />
+          </button>
+          <input name='session' type='text' value='25' disabled />
+          <button id='session-decrement'>
+            <BsCaretDownFill />
+          </button>
+        </div>
       </div>
     </div>
   );
