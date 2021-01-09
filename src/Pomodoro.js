b6560df6a7a129ed94585b86885af0e757c00217
Arz <zarsalan19@gmail.com>
2021-01-09 04:58:23 -0500

    finished project

--- src/Pomodoro.js
+++ src/Pomodoro.js
@@ -7,9 +7,13 @@ function Pomodoro() {
   let [sessionValue, setSessionValue] = useState(25);
   let [timerValue, setTimerValue] = useState('25:00');
   let [isTimerRunning, setIsTimerRunning] = useState(false);
+  let [isSessionRunning, setIsSessionRunning] = useState(true);
 
-  let toSeconds = useRef(0);
+  let sessionLbl = useRef(null);
+  let toSeconds = useRef('25:00');
+  let breakToSeconds = useRef(5);
   let interval = useRef(null);
+  let beep = useRef();
 
   //Convert Minutes to Seconds
   const convertToSeconds = (m) => {
@@ -18,9 +22,34 @@ function Pomodoro() {
 
   //Convert seconds to MM:SS Format
   const setTimer = (seconds) => {
-    return (
-      (seconds - (seconds %= 60)) / 60 + (9 < seconds ? ':' : ':0') + seconds
-    );
+    const test = (seconds - (seconds %= 60)) / 60;
+    return (9 < test ? '' : '0') + test + (9 < seconds ? ':' : ':0') + seconds;
+  };
+
+  //Start the current running Interval
+  const startSessionInterval = () => {
+    toSeconds.current -= 1;
+    setTimerValue(setTimer(toSeconds.current));
+  };
+
+  const startSession = () => {
+    stopInterval(interval.current);
+    toSeconds.current = convertToSeconds(sessionValue);
+    setTimerValue(setTimer(toSeconds.current));
+    interval.current = window.setInterval(startSessionInterval, 1000);
+  };
+
+  const startBreakInterval = () => {
+    breakToSeconds.current -= 1;
+    setTimerValue(setTimer(breakToSeconds.current));
+  };
+
+  //Break Function to set the break time
+  const startBreak = () => {
+    stopInterval(interval.current);
+    breakToSeconds.current = convertToSeconds(breakValue);
+    setTimerValue(setTimer(breakToSeconds.current));
+    interval.current = window.setInterval(startBreakInterval, 1000);
   };
 
   //Stop the current running Interval
@@ -33,14 +62,11 @@ function Pomodoro() {
     setIsTimerRunning(!isTimerRunning);
 
     if (!isTimerRunning) {
-      interval.current = window.setInterval(() => {
-        toSeconds.current -= 1;
-        setTimerValue(setTimer(toSeconds.current));
-        console.log(toSeconds.current);
-        if (toSeconds.current === 0) {
-          stopInterval(interval.current);
-        }
-      }, 1000);
+      if (toSeconds.current !== null) {
+        interval.current = window.setInterval(startSessionInterval, 1000);
+      } else {
+        interval.current = window.setInterval(startBreakInterval, 1000);
+      }
     } else {
       stopInterval(interval.current);
     }
@@ -51,54 +77,96 @@ function Pomodoro() {
     setSessionValue(25);
     setTimerValue('25:00');
     setIsTimerRunning(false);
+    setIsSessionRunning(true);
     toSeconds.current = convertToSeconds(sessionValue);
+    breakToSeconds.current = convertToSeconds(breakValue);
+    beep.current.pause();
+    beep.current.currentTime = 0;
+
     stopInterval(interval.current);
   };
 
   const incrementBreak = () => {
-    if (breakValue < 60) {
+    if (breakValue < 60 && !isTimerRunning) {
       setBreakValue((breakValue += 1));
     }
   };
 
   const decrementBreak = () => {
-    if (breakValue > 1) {
+    if (breakValue > 1 && !isTimerRunning) {
       setBreakValue((breakValue -= 1));
     }
   };
 
   const incrementSession = () => {
-    if (sessionValue < 60) {
+    if (sessionValue < 60 && !isTimerRunning) {
       setSessionValue((sessionValue += 1));
     }
   };
 
   const decrementSession = () => {
-    if (sessionValue > 1) {
+    if (sessionValue > 1 && !isTimerRunning) {
       setSessionValue((sessionValue -= 1));
     }
   };
 
+  //Run only for tracking toSeconds
   useEffect(() => {
-    //set the toSeconds to the converted seconds
-    toSeconds.current = convertToSeconds(sessionValue);
+    if (toSeconds.current === 0) {
+      setIsSessionRunning(false);
+      beep.current.play();
 
-    //set timer to the formatted seconds
-    setTimerValue(setTimer(toSeconds.current));
-    console.log(toSeconds.current);
-    console.log('useEffect');
-  }, [sessionValue]);
+      toSeconds.current = null;
+      startBreak();
+    }
+
+    if (breakToSeconds.current === 0) {
+      beep.current.play();
+      setIsSessionRunning(true);
+      breakToSeconds.current = null;
+      startSession();
+    }
+  }, [startBreak, startSession, timerValue]);
+
+  useEffect(() => {
+    if (isSessionRunning) {
+      //set the toSeconds to the converted sessionValue seconds
+      toSeconds.current = convertToSeconds(sessionValue);
+
+      //set timer to the formatted seconds
+      setTimerValue(setTimer(toSeconds.current));
+    }
+  }, [sessionValue, isSessionRunning]);
+
+  useEffect(() => {
+    //set the toSeconds to the converted sessionValue seconds
+    if (!isSessionRunning) {
+      breakToSeconds.current = convertToSeconds(breakValue);
+
+      //set timer to the formatted seconds
+      setTimerValue(setTimer(breakToSeconds.current));
+    }
+  }, [breakValue, isSessionRunning]);
 
   return (
     <div className='bg-container'>
+      <p>
+        <span className='line'></span>Pomodoro Clock
+        <span className='line'></span>
+      </p>
       <div className='container'>
         {/* Timer value */}
         <div className='timer-container'>
-          <label id='timer-label' htmlFor='timer'>
-            Session
+          <label id='timer-label' htmlFor='timer' ref={sessionLbl}>
+            {isSessionRunning ? 'Session' : 'Break'}
           </label>
-          <input name='timer' type='text' value={timerValue} disabled />
+          <p id='time-left'>{timerValue}</p>
 
+          <audio
+            id='beep'
+            ref={beep}
+            src='https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav'
+          ></audio>
           {/* START AND RESET */}
           <div className='btn-container'>
             <button id='start_stop' onClick={startPause}>
@@ -118,7 +186,13 @@ function Pomodoro() {
           <button id='break-increment' onClick={incrementBreak}>
             <BsCaretUpFill />
           </button>
-          <input name='break' type='text' value={breakValue} disabled />
+          <input
+            id='break-length'
+            name='break'
+            type='text'
+            value={breakValue}
+            disabled
+          />
           <button id='break-decrement' onClick={decrementBreak}>
             <BsCaretDownFill />
           </button>
@@ -126,11 +200,19 @@ function Pomodoro() {
 
         {/* Session Length Values */}
         <div className='session-container'>
-          <label htmlFor='session-label'>Session Length</label>
+          <label id='session-label' htmlFor='session-label'>
+            Session Length
+          </label>
           <button id='session-increment' onClick={incrementSession}>
             <BsCaretUpFill />
           </button>
-          <input name='session' type='text' value={sessionValue} disabled />
+          <input
+            id='session-length'
+            name='session'
+            type='text'
+            value={sessionValue}
+            disabled
+          />
           <button id='session-decrement' onClick={decrementSession}>
             <BsCaretDownFill />
           </button>
